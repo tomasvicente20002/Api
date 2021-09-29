c98b6482d15863ce11f72e37117246a1cb205a06
Tomás vicente <56982030+tomasvicente20002@users.noreply.github.com>
2021-09-29 21:44:57 +0100

    Update pytest.py

--- pytest.py
+++ pytest.py
@@ -1,12 +1,1 @@
-# content of myinvoke.py
-import pytest
-import sys
-
-
-class MyPlugin:
-    def pytest_sessionfinish(self):
-        print("*** test run reporting finishing")
-
-
-if __name__ == "__main__":
-    sys.exit(pytest.main(["-qq"], plugins=[MyPlugin()]))
+print("hello wordl")
