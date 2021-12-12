068647b5020ba0b35b0ba1e962ea5d26b6e75afc
tomasvicente20002 <56982030+tomasvicente20002@users.noreply.github.com>
2021-12-12 19:58:10 +0000

    Update __init__.py

--- App/__init__.py
+++ App/__init__.py
@@ -7,7 +7,7 @@ import App.config as config
 from App.Tables import dal
 
 
-app = Flask(__name__)
+app = application = Flask(__name__)
 configuration = config.Configuration()
 conection = dal.SqlLiteConection(configuration)
 
