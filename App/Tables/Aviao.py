3e50255f9d69f9694b4812d4657f55f3a3c29b27
viniciius14 <rodri.f.ribeiro@gmail.com>
2021-12-14 22:53:54 +0000

    commit

--- App/Tables/Aviao.py
+++ App/Tables/Aviao.py
@@ -11,3 +11,27 @@ class Aeroporto(dal.Table):
         }
         )
 
+    from App.Tables import dal
+
+    def get_id_aviao(self):
+        return self.get_field_value('ID_Aviao') 
+    def set_id_aviao(self,value):
+        self.set_field_value('ID_Aviao',value) 
+
+    id_aviao = property(get_id_aviao,set_id_aviao)
+
+
+    def get_nome_aviao(self):
+        return self.get_field_value('Nome_Aviao') 
+    def set_nome_aviao(self,value):
+        self.set_field_value('Nome_Aviao',value)
+
+    nome_aeroporto = property(get_nome_aviao,set_field_value)
+
+
+    def get_tipo_aviao(self):
+        return self.get_field_value('Tipo_Aviao') 
+    def set_tipo_aviao(self,value):
+        self.set_field_value('Tipo_Aviao',value) 
+
+    id_aviao = property(get_tipo_aviao,set_tipo_aviao)
