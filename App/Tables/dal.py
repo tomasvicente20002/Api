e45cb4092720ef1203239db47e628a12df6fe2fc
tomasvicente20002 <56982030+tomasvicente20002@users.noreply.github.com>
2021-12-14 23:19:28 +0000

    Implementação do insert completa

--- App/Tables/dal.py
+++ App/Tables/dal.py
@@ -1,5 +1,7 @@
-﻿import sqlite3
+﻿from os import W_OK
+import sqlite3
 from sqlite3.dbapi2 import apilevel
+from typing import Dict
 
 class SqlLiteConection():
     def __init__(self,config):
@@ -26,11 +28,11 @@ class SqlLiteConection():
     def roolback(self):
         self.conection.rollback()
 
-    def execute_non_query(self,query,params = None):
-        if(params == None):
-            self.conection.executescript(query)
-        else:
-            self.conection.executescript(query,params)
+    def execute_non_query(self,query):
+        self.conection.executescript(query)
+
+
+
             
     def execute_query(self,query,params = None):
         if(params == None):
@@ -40,6 +42,11 @@ class SqlLiteConection():
             
         return cur
 
+    def execute_scalar(self,query,params = None):
+        rows = self.execute_query(query,params)
+        for row in rows:
+            return row[0]
+
 
 class Field:
     def __init__(self,type,column_name,help = '',pk = False):
@@ -47,6 +54,8 @@ class Field:
         self.column_name = column_name
         self.is_pk = pk
         self.help = help
+        self.value = self.type()
+        
 
 
     def get_value(self):
@@ -60,36 +69,47 @@ class Field:
 
 
 class Table:
-    def __init__(self,name,fields):
+    def __init__(self,name,fields:Dict[str,Field]):
         self.fields = fields
-        self.table_name = name
+        self.table_name = name        
+        self.pk_field_key = ''
+
+        for key in fields.keys():
+            if(fields[key].is_pk):
+                self.pk_field_key = key
+
+
 
     def get_field_value(self,name):
         return self.fields[name].value
     
     def set_field_value(self,name,value):
-        self.field[name].value = value
+        self.fields[name].value = value
 
-    def get_new_pk_id():
-        query = 'select max()'
+    def set_new_pk_id(self, conection:SqlLiteConection):
+        column = self.fields[self.pk_field_key].column_name
+        query = f'select IFNULL(max({column}),0) + 1 from {self.table_name}'
+        var =  conection.execute_scalar(query)
+        self.fields[self.pk_field_key].set_value(var)      
+
 
     def insert(self,conection:SqlLiteConection):
-
-        sel
-
+        self.set_new_pk_id(conection)
         insert_query = f'INSERT INTO [{self.table_name}] ('  
         values_query = 'VALUES('
+        values = []
 
-        for  fields in self.fields.keys():
-            insert_query += f'{fields}, '
+        for  field_key in self.fields.keys():
+            insert_query += f'{field_key}, '
             values_query += '?, '
+            values.append(self.fields[field_key].get_value())
 
 
         insert_query = insert_query[:-2] + ') ' 
         insert_query += values_query [:-2] + ')'
 
         conection.open_transaction()
-        conection.execute(insert_query, self.fields.values())
+        conection.execute_query(insert_query, tuple(values))
         conection.commit()
 
     @staticmethod
